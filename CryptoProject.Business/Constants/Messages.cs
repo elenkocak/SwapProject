576846b2b0bdcf0ad57d4c892359aa6dc5eb1e7e
elen.kocak@coino.com <elen.kocak@coino.com>
2023-01-05 17:45:17 +0300

    Add project files.

---
+++ CryptoProject.Business/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapProject.Business.Constants
+{
+    public class Messages
+    {
+        public static string success = "api_success";
+        public static string unknown_err = "api_err_unknown";
+        public static string not_found = "not_found";
+        public static string err_null = "api_err_null";
+        public static string operation_fail = "operation_error";
+
+
+
+    }
+}
---
+++ CryptoProject.DataAccess/Concrete/Context/SwapDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SwapProject.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoProject.DataAccess.Concrete.Context
+{
+    public class SwapDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=coinodb-dev.cjq6i1xxy6zz.eu-central-1.rds.amazonaws.com;Database=ElenSwapDb;Uid=sa;Password=DtzsCI3HF9n4WIX7O3dj6SSdC43PdpwpMtcaXtDlj8TJy3KDSJ;TrustServerCertificate=True");
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
